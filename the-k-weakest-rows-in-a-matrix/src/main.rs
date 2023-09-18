acfc6c46faad534992cb4cf8e1575dbfe9c00f08
alevani <vanini.alexandre@gmail.com>
2023-09-18 15:44:28 +0200

    Optimize solution with priority queue (Binary Heap)

--- the-k-weakest-rows-in-a-matrix/src/main.rs
+++ the-k-weakest-rows-in-a-matrix/src/main.rs
@@ -1,36 +1,52 @@
-use std::collections::HashMap;
-
 fn main() {
     println!("Hello, world!");
     let solution = Solution::k_weakest_rows(
         vec![vec![1, 0, 0, 1], vec![1, 1, 1, 1], vec![0, 0, 0, 1]],
         2,
     );
+    // 0: 2, 1: 4, 2: 1/_/
 
     println!("{solution:?}");
 }
-
 struct Solution;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    n_soldier: i32,
+    position: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if other.n_soldier == self.n_soldier {
+            other.position.cmp(&self.position)
+        } else {
+            self.n_soldier.cmp(&other.n_soldier)
+        }
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+use std::collections::BinaryHeap;
+
 impl Solution {
     pub fn k_weakest_rows(mat: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
-        // The index is the solider count, the value is the index in the mat vec
-        let mut storage: HashMap<i32, i32> = HashMap::new();
+        let mut heap: BinaryHeap<State> = BinaryHeap::new();
 
         mat.iter().enumerate().for_each(|(i, row)| {
-            if storage.get(&(i as i32)).is_none() {
-                storage.insert(row.iter().sum(), i as i32);
-            }
+            heap.push(State {
+                n_soldier: -row.iter().sum::<i32>(),
+                position: i,
+            })
         });
 
-        let mut solution: Vec<i32> = Vec::new();
-        let mut counter = 0;
-        while solution.len() != k as usize {
-            if let Some(value) = storage.get(&counter) {
-                solution.push(value.to_owned());
-            }
-            counter += 1;
-        }
-
-        solution
+        (0..k)
+            .map(|_| heap.pop().unwrap().position as i32)
+            .collect::<Vec<i32>>()
     }
 }
