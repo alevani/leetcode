4bb3eebc8927f6221584c8269a7605589b71a8a2
alevani <vanini.alexandre@gmail.com>
2023-10-08 20:50:14 +0200

    Finish but won't pass due to itertools hihi

--- build-array-where-you-can-find-the-maximum-exactly-k-comparisons/src/main.rs
+++ build-array-where-you-can-find-the-maximum-exactly-k-comparisons/src/main.rs
@@ -7,26 +7,28 @@ fn main() {
 
 struct Solution;
 impl Solution {
+    pub fn search_function(a: &Vec<&i32>) -> i32 {
+        let mut max_value = -1;
+        let mut max_index = -1_i32;
+        let mut search_cost = 0;
+        a.iter().enumerate().for_each(|(i, b)| {
+            if max_value < **b {
+                max_value = **b;
+                max_index = i as i32;
+                search_cost += 1;
+            }
+        });
+
+        search_cost
+    }
+
     pub fn num_of_arrays(n: i32, m: i32, k: i32) -> i32 {
         let items = (1..=m).collect::<Vec<i32>>();
-
-      
-        for a in std::iter::repeat(items.iter())
-        .take(n as usize)
-        .multi_cartesian_product()
-        .filter(|a| {
-            a.iter().enumerate()
-            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-            // .map(|b| {println!("{b:?}"); b})
-            .map(|(index, _)| {println!("{index:?}"); index}).expect("Partial compare failed") < k as usize
-        })
-         {
-            println!("{a:?}");
-        }
 
         std::iter::repeat(items.iter())
             .take(n as usize)
             .multi_cartesian_product()
+            .filter(|a| self::Solution::search_function(a) <= k)
             .count() as i32
     }
 }
