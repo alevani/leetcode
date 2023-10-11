4eec7fd5cb1e3721b08f9a1b4441f8228347adac
alevani <vanini.alexandre@gmail.com>
2023-10-11 14:09:25 +0200

    Optimizations

--- minimum-number-of-operations-to-make-array-continuous/src/main.rs
+++ minimum-number-of-operations-to-make-array-continuous/src/main.rs
@@ -1,29 +1,39 @@
 use std::collections::HashMap;
 
 fn main() {
-    println!("{:?}", Solution::min_operations(vec![1,10,100,1000]));
+    println!(
+        "{:?}",
+        Solution::min_operations(vec![41, 33, 29, 33, 35, 26, 47, 24, 18, 28])
+    );
 }
 
 struct Solution;
 impl Solution {
     pub fn min_operations(mut nums: Vec<i32>) -> i32 {
-        let len = nums.len();
-        nums.sort();
-    
-        
-        //[1,10,100,1000]
-        let mut duplicate = false;
-        for i in 1..len {
-            // Duplicate
-            if nums[i - 1] == nums[i] {
-                duplicate = true;
-            }
-        }
+        let mut duplicate_memory = HashMap::<usize, i32>::new();
 
-        if nums.iter().max().unwrap() - nums.iter().min().unwrap() == len as i32 && !duplicate {
-            0
-        } else {
-            2
-        }
+        nums.sort();
+        println!("{nums:?}");
+        let max = (nums.len() - 1) as i32 + nums[0];
+        let operations = nums
+            .clone()
+            .into_iter()
+            .filter(|x| x > &max)
+            .collect::<Vec<_>>()
+            .len();
+
+        nums.iter()
+            .for_each(|e| *duplicate_memory.entry(*e as usize).or_insert(0) += 1);
+
+        println!("{duplicate_memory:?}");
+
+        let m = duplicate_memory
+            .into_iter()
+            .filter(|&(key, value)| key <= max as usize && value > 1)
+            .collect::<HashMap<_, _>>();
+        
+        let b = m.values();
+        
+        ((nums.len()/2) as i32).min(operations as i32 + b.clone().sum::<i32>() - b.collect::<Vec<&i32>>().len() as i32)
     }
 }
--- number-of-flowers-in-full-bloom/src/main.rs
+++ number-of-flowers-in-full-bloom/src/main.rs
@@ -13,14 +13,68 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn full_bloom_flowers(flowers: Vec<Vec<i32>>, people: Vec<i32>) -> Vec<i32> {
+        let mut first = Vec::new();
+        let mut second = Vec::new();
+
+        for inner in flowers {
+            if let [a, b] = inner.as_slice() {
+                first.push(*a);
+                second.push(*b);
+            }
+        }
+
+        first.sort();
+        second.sort();
+
+        fn binary_search(into: Vec<i32>, index: &i32) -> i32 {
+            let mut len = into.len();
+            let mut mid;
+
+            let mut memory = into.clone();
+            let mut index_memory = 0;
+
+            println!("INDEX: ____{index}");
+            while len > 1 {
+                println!("len {len}");
+                
+                mid = len / 2 - 1;
+                println!("mid {mid}"); 
+                index_memory += mid;
+                
+                if &memory[mid] == index {
+                    println!("yeee 1");
+                    println!("{memory:?}");
+                    println!("exiting index_memory: {index_memory:?}");
+                    return index_memory as i32; // todo index_memory 
+                } 
+                else if &memory[mid] < index && &memory[mid + 1] > index {
+                    println!("yeee 2");
+                    println!("{memory:?}");
+                    println!("exiting index_memory: {index_memory:?}");
+                    return index_memory as i32 + 1; // todo index_memory 
+                }
+
+                else if &memory[mid] > index {
+                    memory = memory[0..=mid].to_vec();
+                    index_memory -= mid;
+                } else {
+                    memory = memory[(mid + 1)..].to_vec();
+                    index_memory += 1;
+                }
+                println!("index_memory: {index_memory:?}");
+                println!("{memory:?}");
+                
+                len = memory.len();
+            }
+            
+            println!("yeee 3");
+            println!("exiting index_memory: {index_memory:?}");
+            index_memory as i32 
+        }
+
         people
             .iter()
-            .map(|t| {
-                flowers
-                    .iter()
-                    .map(|f| if &f[0] <= t { 1 } else { 0 } - if &f[1] < t { 1 } else { 0 })
-                    .sum::<i32>()
-            })
+            .map(|t| binary_search(first.clone(), t) - binary_search(second.clone(), t))
             .collect()
     }
 }
